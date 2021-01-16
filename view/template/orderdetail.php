bf7a712bd76ff99dcb7e0a6caa35177ad685a6ac
Andrea Giulianelli <andrygiulio@gmail.com>
2021-01-17 00:20:05 +0100

    Add order detail

---
+++ view/template/orderdetail.php
@@ -0,0 +1,72 @@
+<section>
+    <header>
+        <div class="row">
+            <div class="col-12">
+                    <h1 class="text-left col-title font-weight-lighter">Dettaglio</h1>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <h2 class="mt-3 col-text font-weight-light">Informazioni generali</h2>
+                <div class="rounded my-2 col-back-white p-4 col-dark ">
+                    <img src="/spaceair/res/icons/luna.png" class="planet-img mw-25 float-right mr-4" alt="">
+                    <p class="my-0 text-uppercase font-weight-bold list-impo-text">Venere</p>
+                    <p>20.11.2021 - 14:46</p>
+                    <p class="text-uppercase my-0">Andrea Giulianelli</p>
+                    <p class="font-weight-normal my-0">15.03.2021</p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="offset-1 col-10 col-md-6 offset-md-3">
+                <p class="font-weight-bold col-text my-0">Data partenza:<span class="float-right font-weight-normal">20/11/2021</span></p>
+                <p class="font-weight-bold col-text">Ora partenza:<span class="float-right font-weight-normal">14:46</span></p>
+                <p class="font-weight-bold col-text my-0">Data arrivo:<span class="float-right font-weight-normal">22/11/2022</span></p>
+            </div>
+        </div>
+    </section>    
+    <section class="mt-5">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <h2 class="col-text font-weight-light">QR Code</h2>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-12 col-md-6 offset-md-3 text-center">
+                <!-- GENERARE QR CODE -->
+                <!-- metto per ora un'immagine solo per prova -->
+                <img src="/spaceair/res/icons/test_qrcode.png" class="bg-light w-50" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="mt-5">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <h2 class="col-text font-weight-light">Utilit&agrave;</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">
+                        <a class="col-btn-impo nav-link rounded col-text" href="#">
+                            Tracking
+                            <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri i miei ordini">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="row mt-4">
+        <div class="col-12">
+            <div class="fixed-bottom px-4 py-3 col-btn-regular col-blue">
+                <p class="float-right font-weight-bolder impo-text my-0">$7500</p>
+                <p class="font-weight-bolder list-impo-text my-0">Quantit&agrave;: 2</p>
+                <p class="font-weight-lighter my-0">+ Biglietto personalizzato</p>
+            </div>
+        </div>
+    </div>
+</section>
