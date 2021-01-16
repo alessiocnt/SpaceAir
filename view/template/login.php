eb8d3a3ac0d83a409c1953672bd39de33a9f87cb
Andrea Giulianelli <andrygiulio@gmail.com>
2021-01-16 22:53:44 +0100

    Add login and profile page

---
+++ view/template/login.php
@@ -0,0 +1,31 @@
+<section>
+    <header>
+        <div class="row">
+            <div class="col-12">
+                    <h1 class="text-left col-title font-weight-lighter">Login</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center p-4">
+                <img src="/spaceair/res/icons/decorative/undraw_maker_launch_crhe.svg" alt="" class="decorative-img mw-50">
+            </div>
+        </div>
+    </header>
+    <div class="row mt-2">
+        <div class="offset-1 col-10 offset-md-4 col-md-4">
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="email" class="col-text">Email</label>
+                    <input type="email" class="col-back-white form-control" name="email" id="email" autocomplete="on" required/>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-text">Password</label>
+                    <input type="password" class="col-back-white form-control" name="password" id="password" required/>
+                </div>
+
+                <input type="submit" class="mt-2 py-2 col-12 btn col-btn-impo col-title font-weight-light" value="Login"/> 
+            </form>
+            <p class="mt-1 col-text font-weight-light">Non hai un account? <a href="#" class="col-link">Registrati!</a></p>
+        </div>
+    </div>
+</section>
--- view/template/signup.php
+++ view/template/signup.php
@@ -1,93 +1,94 @@
 <section>
-    <div class="row">
-        <div class="col-12">
-                <h1 class="text-left col-title font-weight-lighter">Registrazione</h1>
+    <header>
+        <div class="row">
+            <div class="col-12">
+                    <h1 class="text-left col-title font-weight-lighter">Registrazione</h1>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12 text-center p-4">
-            <img src="/spaceair/res/icons/decorative/undraw_Outer_space_drqu.svg" alt="" class="decorative-img mw-50">
+        <div class="row">
+            <div class="col-12 text-center p-4">
+                <img src="/spaceair/res/icons/decorative/undraw_Outer_space_drqu.svg" alt="" class="decorative-img mw-50">
+            </div>
         </div>
-    </div>
+    </header>
     <div class="row mt-2">
         <div class="offset-1 col-10 offset-md-4 col-md-4">
-            <section>
-                <form action="" method="POST">
-                    <fieldset class="form-group"><legend class="col-title">Dati Utente</legend>
-                    <div class="form-group">
-                        <label for="name" class="col-text">Nome</label>
-                        <input type="text" class="col-back-white form-control" name="name" id="name" autocomplete="on" required/>
-                    </div>
-                    <div class="form-group">
-                        <label for="surname" class="col-text">Cognome</label>
-                        <input type="text" class="col-back-white form-control" name="surname" id="surname" autocomplete="on" required/>
-                    </div>
-                    <div class="form-group">
-                        <label for="borndate" class="col-text">Data di Nascita</label>
-                        <input type="date" class="col-back-white form-control" name="borndate" id="borndate" autocomplete="on" required/>
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="col-text">Email</label>
-                        <input type="email" class="col-back-white form-control" name="email" id="email" autocomplete="on" required/>
-                    </div>
-                    <div class="form-group">
-                        <label for="password" class="col-text">Password</label>
-                        <input type="password" class="col-back-white form-control" name="password" id="password" required/>
-                    </div>
-                    <div class="form-group">
-                        <label for="confirmpassword" class="col-text">Conferma Password</label>
-                        <input type="password" class="col-back-white form-control" name="confirmpassword" id="confirmpassword" required/>
-                    </div>
-                    </fieldset>
+            <form action="" method="POST">
+                <fieldset class="form-group"><legend class="col-title">Dati Utente</legend>
+                <div class="form-group">
+                    <label for="name" class="col-text">Nome</label>
+                    <input type="text" class="col-back-white form-control" name="name" id="name" autocomplete="on" required/>
+                </div>
+                <div class="form-group">
+                    <label for="surname" class="col-text">Cognome</label>
+                    <input type="text" class="col-back-white form-control" name="surname" id="surname" autocomplete="on" required/>
+                </div>
+                <div class="form-group">
+                    <label for="borndate" class="col-text">Data di Nascita</label>
+                    <input type="date" class="col-back-white form-control" name="borndate" id="borndate" autocomplete="on" required/>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-text">Email</label>
+                    <input type="email" class="col-back-white form-control" name="email" id="email" autocomplete="on" required/>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-text">Password</label>
+                    <input type="password" class="col-back-white form-control" name="password" id="password" required/>
+                </div>
+                <div class="form-group">
+                    <label for="confirmpassword" class="col-text">Conferma Password</label>
+                    <input type="password" class="col-back-white form-control" name="confirmpassword" id="confirmpassword" required/>
+                </div>
+                </fieldset>
 
-                    <fieldset class="form-group">
-                        <legend class="col-text">Indirizzo di Default</legend>
-                        <div class="form-row">                            
-                            <div class="form-group col-7">
-                                <label for="address" class="col-text">Via</label>
-                                <input type="text" class="col-back-white form-control" name="address" id="address" autocomplete="on" required/>
-                            </div>
-                            <div class="form-group offset-1 col-4">
-                                <label for="civico" class="col-text">Civico</label>
-                                <input type="text" class="col-back-white form-control" name="civico" id="civico" autocomplete="on" required/>
-                            </div>
+                <fieldset class="form-group">
+                    <legend class="col-text">Indirizzo di Default</legend>
+                    <div class="form-row">                            
+                        <div class="form-group col-7">
+                            <label for="address" class="col-text">Via</label>
+                            <input type="text" class="col-back-white form-control" name="address" id="address" autocomplete="on" required/>
                         </div>
-                        <div class="form-group">
-                            <label for="city" class="col-text">Citt&agrave</label>
-                            <input type="text" class="col-back-white form-control" name="city" id="city" autocomplete="on" required/>
+                        <div class="form-group offset-1 col-4">
+                            <label for="civico" class="col-text">Civico</label>
+                            <input type="text" class="col-back-white form-control" name="civico" id="civico" autocomplete="on" required/>
                         </div>
-                        <div class="form-row">
-                            <div class="form-group col-7">
-                                <label for="provincia" class="col-text">Provincia</label>
-                                <input type="text" class="col-back-white form-control" name="provincia" id="provincia" autocomplete="on" required/>
-                            </div>
-                            <div class="form-group offset-1 col-4">
-                                <label for="cap" class="col-text">CAP</label>
-                                <input type="number" min=0 step="1" class="col-back-white form-control" name="cap" id="cap" autocomplete="on" required/>
-                            </div>
-                        </div>
-                    </fieldset>
-
-                    <fieldset class="form-group">
-                        <legend class="col-text">Immagine di profilo</legend>
-                        <div class="row">
-                            <div class="col-12 text-center mt-1 mb-4">
-                                <img src="/spaceair/res/icons/decorative/user.jpeg" class="decorative-img rounded-circle mw-50" alt="Immagine selezionata"/>
-                            </div>
-                        </div>
-                        <div class="custom-file">
-                            <label for="img" class="custom-file-label">Clicca per inserire</label>
-                            <input type="file" class="custom-file-input col-back-white" name="img" id="img"/>
-                        </div>
-                    </fieldset>
-
-                    <div class="custom-control custom-checkbox mt-5">
-                        <input type="checkbox" class="custom-control-input" name="newsletter" id="newsletter">
-                        <label class="col-text font-weight-light custom-control-label" for="newsletter">Accetto di ricevere la newsletter</label>
                     </div>
-                    <input type="submit" class="mt-2 py-2 col-12 btn col-btn-impo col-title font-weight-light" value="Registrati"/> 
-                </form>
-                <p class="mt-1 col-text font-weight-light">Hai gi&agrave un account? <a href="#" class="col-link">Login</a></p>
-            </section>
+                    <div class="form-group">
+                        <label for="city" class="col-text">Citt&agrave;</label>
+                        <input type="text" class="col-back-white form-control" name="city" id="city" autocomplete="on" required/>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-7">
+                            <label for="provincia" class="col-text">Provincia</label>
+                            <input type="text" class="col-back-white form-control" name="provincia" id="provincia" autocomplete="on" required/>
+                        </div>
+                        <div class="form-group offset-1 col-4">
+                            <label for="cap" class="col-text">CAP</label>
+                            <input type="number" min=0 step="1" class="col-back-white form-control" name="cap" id="cap" autocomplete="on" required/>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset class="form-group">
+                    <legend class="col-text">Immagine di profilo</legend>
+                    <div class="row">
+                        <div class="col-12 text-center mt-1 mb-4">
+                            <img src="/spaceair/res/icons/decorative/user.jpeg" class="decorative-img rounded-circle mw-50" alt="Immagine selezionata"/>
+                        </div>
+                    </div>
+                    <div class="custom-file">
+                        <label for="img" class="custom-file-label">Clicca per inserire</label>
+                        <input type="file" class="custom-file-input col-back-white" name="img" id="img"/>
+                    </div>
+                </fieldset>
+
+                <div class="custom-control custom-checkbox mt-5">
+                    <input type="checkbox" class="custom-control-input" name="newsletter" id="newsletter">
+                    <label class="col-text font-weight-light custom-control-label" for="newsletter">Accetto di ricevere la newsletter</label>
+                </div>
+                <input type="submit" class="mt-2 py-2 col-12 btn col-btn-impo col-title font-weight-light" value="Registrati"/> 
+            </form>
+            <p class="mt-1 col-text font-weight-light">Hai gi&agrave; un account? <a href="#" class="col-link">Login</a></p>
         </div>
+    </div>
 </section>
