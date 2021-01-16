60318409b65c436c4a39fdcadfdea9b9fe2f7f95
Andrea Giulianelli <andrygiulio@gmail.com>
2021-01-16 23:08:28 +0100

    Fix Desktop profile

---
+++ view/template/myorders.php
@@ -0,0 +1,14 @@
+<section>
+    <header>
+        <div class="row">
+            <div class="col-12">
+                    <h1 class="text-left col-title font-weight-lighter">I miei ordini</h1>
+            </div>
+        </div>
+    </header>
+    <div class="row">
+        <div class="col-12">
+            
+        </div>
+    </div>
+</section>
--- view/template/profile.php
+++ view/template/profile.php
@@ -16,43 +16,47 @@
             <p class="col-text font-weight-bolder">Andrea</p>
         </div>
     </div>
-    <div class="row mt-4">
-        <div class="offset-1 col-10">
-            <ul class="nav flex-column">
-                <li class="nav-item my-2">
-                    <a class="col-back-white nav-link rounded col-dark" href="#">
-                        I miei ordini
-                        <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri i miei ordini">
-                    </a>
-                </li>
-                <li class="nav-item my-1">
-                    <a class="col-back-white nav-link rounded col-dark" href="#">
-                        I miei indirizzi di consegna
-                        <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri i miei indirizzi di consegna">
-                    </a>
-                </li>
-                <li class="nav-item my-2">
-                    <a class="col-back-white nav-link rounded col-dark" href="#">
-                        Impostazioni
-                        <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri impostazioni">
-                    </a>
-                </li>
-            </ul>
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <div class="row mt-4">
+                <div class="offset-1 col-10">
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2">
+                            <a class="col-back-white nav-link rounded col-dark" href="#">
+                                I miei ordini
+                                <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri i miei ordini">
+                            </a>
+                        </li>
+                        <li class="nav-item my-1">
+                            <a class="col-back-white nav-link rounded col-dark" href="#">
+                                I miei indirizzi di consegna
+                                <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri i miei indirizzi di consegna">
+                            </a>
+                        </li>
+                        <li class="nav-item my-2">
+                            <a class="col-back-white nav-link rounded col-dark" href="#">
+                                Impostazioni
+                                <img class="mt-1 float-right" src="/spaceair/res/icons/navigate_next-black-18dp.svg" alt="apri impostazioni">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <!-- I miei interessi -->
+            <div class="row mt-4">
+                <div class="offset-1 col-10">
+                    <section class="rounded col-dark bg-light p-4">
+                        <h2>I miei interessi<span class="float-right"><img src="/spaceair/res/icons/edit-black-18dp.svg" alt="Modifica Interessi"></span></h2>
+                        <ul class="mt-3">
+                            <li>Luna 10/12/2020</li>
+                            <li>Marte 25/07/2021</li>
+                            <li>Giove 30/11/2021</li>
+                        </ul>
+                    </section>    
+                </div>
+            </div>
         </div>
     </div>
 </section>
-<aside>
-<!-- I miei interessi -->
-<div class="row mt-4">
-    <div class="offset-1 col-10">
-        <section class="rounded col-dark bg-light p-4">
-            <h2>I miei interessi<span class="float-right"><img src="/spaceair/res/icons/edit-black-18dp.svg" alt="Modifica Interessi"></span></h2>
-            <ul class="mt-3">
-                <li>Luna 10/12/2020</li>
-                <li>Marte 25/07/2021</li>
-                <li>Giove 30/11/2021</li>
-            </ul>
-        </section>    
-    </div>
-</div>
-</aside>
