543e6741e414cc203dc23f1ecbf6924e7e197baf
Simone Ceredi <simone.ceredi@studio.unibo.it>
2021-01-17 15:55:13 +0100

    Added required in packetInsert

--- view/template/packetInsert.php
+++ view/template/packetInsert.php
@@ -90,8 +90,8 @@
                             <div class="row mb-3">
                                 <div class="container">
                                     <label for="inputDestination">Destinazione</label>
-                                    <select name="inputDestination" id="inputDestination" class="form-control">
-                                        <option selected>Seleziona...</option>
+                                    <select name="inputDestination" id="inputDestination" class="form-control" required>
+                                        <option value = "" selected hidden>Seleziona...</option>
                                         <option value="Marte">Marte</option>
                                         <option value="Venere">Venere</option>
                                     </select>
@@ -100,17 +100,17 @@
                             <div class="row mb-3">
                                 <div class="col-6">
                                     <label for="inputDate">Data</label>
-                                    <input type="date" class="form-control" name="inputDate" id="inputDate" />
+                                    <input type="date" class="form-control" name="inputDate" id="inputDate" required/>
                                 </div>
                                 <div class="col-6">
                                     <label for="inputHour">Ora partenza</label>
-                                    <input type="time" class="form-control" name="inputHour" id="inputHour" />
+                                    <input type="time" class="form-control" name="inputHour" id="inputHour" required/>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-6">
                                     <label for="inputCapacity">Capienza</label>
-                                    <input type="number" min="0" max="10000" step="1" class="form-control" name="inputCapacity" id="inputCapacity" />
+                                    <input type="number" min="0" max="10000" step="1" class="form-control" name="inputCapacity" id="inputCapacity" required/>
                                 </div>
                                 <div class="col-6">
                                     <label for="inputPrice">Prezzo</label>
@@ -118,7 +118,7 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text">€</span>
                                         </div>
-                                        <input type="number" min="0" step="1" class="form-control" name="inputPrice" id="inputPrice" aria-label="Amount (to the nearest dollar)"/>
+                                        <input type="number" min="0" step="1" class="form-control" name="inputPrice" id="inputPrice" aria-label="Amount (to the nearest dollar)" required/>
                                         <div class="input-group-append">
                                             <span class="input-group-text">.00</span>
                                         </div>
@@ -128,12 +128,16 @@
                             <div class="row mb-3">
                                 <div class="container">
                                     <label for="inputDescription">Descrizione</label>
-                                    <textarea class="form-control" name="inputDescription" id="inputDescription" rows="5"></textarea>
+                                    <textarea class="form-control" name="inputDescription" id="inputDescription" rows="5" required></textarea>
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <div class="col-4 offset-8 col-md-2 offset-md-10">
-                                    <input type="submit" class="form-control" value="Inserisci"/>
+                                <div class="col-2">
+                                    <label for="inputVisible">Visibile</label>
+                                    <input type="checkbox" name="inputDescription" id="inputVisible">
+                                </div>
+                                <div class="col-4 offset-6 col-md-2 offset-md-8">
+                                    <input type="submit" class="form-control" value="Inserisci" />
                                 </div>
                             </div>
                         </form>
