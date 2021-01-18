d434e3f47de7a4e003ffa8d27711ea621cab57fc
Andrea Giulianelli <andrygiulio@gmail.com>
2021-01-18 18:13:52 +0100

    Fix footer

--- view/test/TestView.php
+++ view/test/TestView.php
@@ -4,7 +4,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/spaceair/autoloaders/commonAutoloader
 class TestView extends ViewBaseImpl {
 
     protected function renderMain($data) {
-        require $_SERVER["DOCUMENT_ROOT"] . "/spaceair/view/template/login.php";
+        require $_SERVER["DOCUMENT_ROOT"] . "/spaceair/view/template/packetsbought.php";
     }
 }
 ?>
