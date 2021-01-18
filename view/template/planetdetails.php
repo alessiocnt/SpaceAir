a8eeeee3e36e32a84eb84ed64a688e3ac840fe49
Alessio <alecervia@live.it>
2021-01-18 09:39:02 +0100

    planetdetails

--- view/template/planetdetails.php
+++ view/template/planetdetails.php
@@ -76,17 +76,17 @@
                         <li class="w-100">Giornata 25°C</li>
                     </ul>
                 </div>
-                <p class="col-text mb-0 mt-5">
+                <p class="col-text mb-0 mt-5 pb-3">
                     Troianas ut opes et lamentabile regnum eruerint Danai, quaeque ipse miserrima vidi et quorum pars magna fui. Quis talia fando Myrmidonum Dolopumve aut duri miles Ulixi temperet a lacrimis?
                 </p>
                 <footer>
-                    <button class="btn float-right col-dark col-back-white " type="button">
+                    <button class="btn float-right col-dark col-back-white" type="button">
                         Espandi
                     </button>
                 </footer>
             </article>
 
-            <div class="row">
+            <div class="row mb-3">
                 <div class="col-10 offset-1">
                     <h2 class="mt-3 mb-3 col-text font-weight-light">Voli disponibili</h2>
                     <ul class="list-group">
@@ -118,68 +118,89 @@
                 </div>
             </div>
 
-            <div class="row">
+            <div class="row mb-3">
                 <div class="col-10 offset-1">
                     <h2 class="mt-3 mb-3 col-text font-weight-light">Inserisci la tua recensione</h2>
-                        <form action="">
-                            <div class="row mb-3">
-                                    <div class="col-8">
-                                        <label for="inputTitle">Titolo</label>
-                                        <input type="text" class="form-control" name="inputTitle" id="inputTitle" required/>
-                                    </div>
-                                    <div class="col-4">
-                                        <button class="btn float-right" type="button">
-                                            <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star1">
-                                        </button>
-                                        <button class="btn float-right" type="button">
-                                            <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star2">
-                                        </button>
-                                        <button class="btn float-right" type="button">
-                                            <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star3">
-                                        </button>
-                                        <button class="btn float-right" type="button">
-                                            <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star4">
-                                        </button>
-                                        <button class="btn float-right" type="button">
-                                            <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star5">
-                                        </button>
-                                    </div>
+                    <form action="">
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <label for="inputTitle">Titolo</label>
+                                <input type="text" class="form-control" name="inputTitle" id="inputTitle" required/>
                             </div>
-                            <div class="row mb-3">
-                                    <div class="container">
-                                        <label for="inputReview">Recensione</label>
-                                        <textarea class="form-control" name="inputReview" id="inputReview" rows="5"></textarea>
-                                    </div>
+                            <div class="col-6 py-0">
+                                <button class="btn float-right" type="button">
+                                    <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star 1">
+                                </button>
+                                <button class="btn float-right" type="button">
+                                    <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star 2">
+                                </button>
+                                <button class="btn float-right" type="button">
+                                    <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star 3">
+                                </button>
+                                <button class="btn float-right" type="button">
+                                    <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star 4">
+                                </button>
+                                <button class="btn float-right" type="button">
+                                    <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="scale-x2 img-fluid" alt="star 5">
+                                </button>
                             </div>
-                            <div class="col-3 offset-9">
-                                        <input type="submit" class="form-control" value="Pubblica"/>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="container col-12">
+                                <label for="inputReview">Recensione</label>
+                                <textarea class="form-control" name="inputReview" id="inputReview" rows="5"></textarea>
                             </div>
-                        </form>
-                    
+                        </div>
+                        <button class="btn float-right col-dark col-back-white" type="submit">
+                            Pubblica
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-10 offset-1">
+                    <div class="my-3 px-0 float-right">
+                        <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="img-fluid" alt="star 1">
+                        <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="img-fluid" alt="star 2">
+                        <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="img-fluid" alt="star 3">
+                        <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="img-fluid" alt="star 4">
+                        <img src="/spaceair/res/icons/star_rate-white-18dp.svg" class="img-fluid" alt="star 5">
+                    </div>
+                    <h2 class="my-3 col-text font-weight-light">Recensioni</h2>
                     <ul class="list-group">
                         <li class="list-group-item rounded mb-3 col-back-white py-4">
-                            <button class="btn mt-2 mr-2 float-right" type="button">
-                                <img src="/spaceair/res/icons/shopping_cart-24px.svg" class="scale-x2" alt="Preferiti">
-                            </button>
-                            <img src="/spaceair/res/img/moon.png" class="planet-img mw-25 float-left mr-4" alt="">
-                            <p class="col-dark font-weight-bold list-impo-text my-0">15 febbraio 2021</p>
-                            <p class="col-dark font-weight-normal list-impo-text my-0">$ 7500</p>
+                            <div class="mb-2 px-0 float-right">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 1">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 2">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 3">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 4">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 5">
+                            </div>
+                            <h3 class="font-weight-bold list-impo-text mb-2">Titolo review</h3>
+                            <p class="col-dark my-0">Troianas ut opes et lamentabile regnum eruerint Danai, quaeque ipse miserrima vidi et quorum pars magna fui. Quis talia fando Myrmidonum Dolopumve aut duri miles Ulixi temperet a lacrimis?</p>
                         </li>
                         <li class="list-group-item rounded mb-3 col-back-white py-4">
-                            <button class="btn mt-2 mr-2 float-right" type="button">
-                                <img src="/spaceair/res/icons/shopping_cart-24px.svg" class="scale-x2" alt="Preferiti">
-                            </button>
-                            <img src="/spaceair/res/img/moon.png" class="planet-img mw-25 float-left mr-4" alt="">
-                            <p class="col-dark font-weight-bold list-impo-text my-0">15 febbraio 2021</p>
-                            <p class="col-dark font-weight-normal list-impo-text my-0">$ 7500</p>
+                            <div class="mb-2 px-0 float-right">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 1">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 2">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 3">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 4">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 5">
+                            </div>
+                            <h3 class="font-weight-bold list-impo-text mb-2">Titolo review</h3>
+                            <p class="col-dark my-0">Troianas ut opes et lamentabile regnum eruerint Danai, quaeque ipse miserrima vidi et quorum pars magna fui. Quis talia fando Myrmidonum Dolopumve aut duri miles Ulixi temperet a lacrimis?</p>
                         </li>
                         <li class="list-group-item rounded mb-3 col-back-white py-4">
-                            <button class="btn mt-2 mr-2 float-right" type="button">
-                                <img src="/spaceair/res/icons/shopping_cart-24px.svg" class="scale-x2" alt="Preferiti">
-                            </button>
-                            <img src="/spaceair/res/img/moon.png" class="planet-img mw-25 float-left mr-4" alt="">
-                            <p class="col-dark font-weight-bold list-impo-text my-0">15 febbraio 2021</p>
-                            <p class="col-dark font-weight-normal list-impo-text my-0">$ 7500</p>
+                            <div class="mb-2 px-0 float-right">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 1">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 2">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 3">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 4">
+                                <img src="/spaceair/res/icons/star_rate-24px.svg" class="scale-x075 img-fluid" alt="star 5">
+                            </div>
+                            <h3 class="font-weight-bold list-impo-text mb-2">Titolo review</h3>
+                            <p class="col-dark my-0">Troianas ut opes et lamentabile regnum eruerint Danai, quaeque ipse miserrima vidi et quorum pars magna fui. Quis talia fando Myrmidonum Dolopumve aut duri miles Ulixi temperet a lacrimis?</p>
                         </li>
                     </ul>
                 </div>
