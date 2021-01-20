c4da17147dac675079a48553b25a4c3a9e388d74
Simone Ceredi <simone.ceredi@studio.unibo.it>
2021-01-20 16:36:51 +0100

    Now using secure session start

--- controller/api/AbstractApi.php
+++ controller/api/AbstractApi.php
@@ -5,8 +5,9 @@ abstract class AbstractApi {
     private $db;
 
     public function __construct(){
-        $this->db = new DbManagerImpl("localhost", "root", "", "spaceair", 3306).getDb();
-        session_start();
+        $this->db = new DbManagerImpl("localhost", "root", "", "spaceair", 3306);
+        $this->db = $this->db->getDb();
+        Utils::sec_session_start();
     }
 
     public function getDb() {
