73050ea6e3e4069cb24307bc0f9c25c1c0031100
Andrea Giulianelli <andrygiulio@gmail.com>
2021-01-21 18:43:46 +0100

    Control for logged user in API

---
+++ controller/api/AdminLoggedApi.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    Check login (secure) before use the API.
+    Import in the api and do your staff.
+    WARNING: there is yet the model object, so you don't have to create one
+*/
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/spaceair/autoloaders/commonAutoloader.php");
+
+//Start secure session
+Utils::sec_session_start();
+
+$model = new ModelImpl();
+if(!$model->getUserHandler()->checkLogin(UserHandler::$LOGINOKADMIN)) {
+    echo json_encode(array("Error" => "Not logged"));
+}
+
+?>
