056c806594821734199276e02c3f3c6aab284cd2
Simone Ceredi <simone.ceredi@studio.unibo.it>
2021-01-21 10:25:46 +0100

    Completed cart remove

--- view/js/cart.js
+++ view/js/cart.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
     total();
+    if($('section#cart > article').length > 0) {
+        $("h1").text("Carrello");
+    } else {
+        $("h1").text("Carrello vuoto");
+    }
 
     $("input").change(function(e) {
         $.post("/spaceair/controller/api/CartApi.php/", {"id": e.target.id, "qnt": e.target.value, "cod": e.target.name}, function(data) { 
             $("p#p" + e.target.name).text("Costo €" + data);
             total();
         });
+    });
+
+    $("a").click(function(e) {
+        e.preventDefault();
+        const packet = $(this).attr("class");
+        const order = $(this).attr("id");
+        const qnt = 0;
+        $.post("/spaceair/controller/api/CartApi.php/", {"id": packet, "qnt": qnt, "cod": order}, function(data) { 
+            $("p#p" + order).text("Costo €" + data);
+            total();
+        });
+        $(this).parents("article").hide();
     });
 
     function total() {
