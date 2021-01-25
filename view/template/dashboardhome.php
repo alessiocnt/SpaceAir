16b83a1ba859ecaa4e9bcca80832e31b656c5070
Andrea Giulianelli <andrygiulio@gmail.com>
2021-01-25 14:40:17 +0100

    Fix logout btn

--- view/template/dashboardhome.php
+++ view/template/dashboardhome.php
@@ -81,7 +81,7 @@
                     </div>
                     <div class="row">
                             <div class="col-12">
-                                <a class="mt-5 btn col-btn-impo col-text offset-1 col-4 col-md-2" href="?logout=1" role="button">Logout</a>
+                                <a class="mt-5 btn col-btn-impo col-text offset-1 col-5" href="?logout=1" role="button">Logout</a>
                             </div>
                     </div>
             </section>
