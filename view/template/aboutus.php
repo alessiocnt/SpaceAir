ba003b0ec6aedd41ea950aff4e0dcad922facb41
Alessio <alecervia@live.it>
2021-01-25 19:32:33 +0100

    about us

--- view/template/aboutus.php
+++ view/template/aboutus.php
@@ -13,12 +13,28 @@
         </div>
     </section>
     <section class="row col-10 offset-1 mb-3 col-md-6 offset-md-3 p-0">
-        <div class="row">
+        <div class="row col-12">
+            <p class="font-weight-lighter col-text my-0">Aperto tutti i giorni 24/7</p>    
+        </div>
+        <div class="row col-12">
+            <p class="font-weight-lighter col-text my-0">Telefono: 333-1212123</p>
+        </div>
+        <div class="row col-12">
+            <p class="font-weight-lighter col-text my-0">Indirizzo: Via dell’Università 14, Cesena (FC)</p>
+        </div>
+    </section>  
+    <!--Google map-->
+    <section class="col-10 offset-1 mb-3 col-md-6 offset-md-3">
+        <div id="map-google" class="z-depth-1-half map-container">
+            <iframe src="https://maps.google.com/maps?q=Campus+di+Cesena+-+Università+di+Bologna+-+Alma+Mater+Studiorum&t=&z=13&ie=UTF8&iwloc=&output=embed" allowfullscreen></iframe>
+        </div>  
+    </section>    
+    <!--Contatti-->
+    <section class="row col-10 offset-1 mb-3 col-md-6 offset-md-3 p-0">
+        <div class="col-12 p-0">
             <div class="row col-12">
-                <div class="col-8">
-                    <p class="font-weight-lighter col-text my-0">Aperto tutti i giorni 24/7</p>
-                </div>
-                <div class="col-4 p-0">
+                <h2 class="col-6 mt-3 mb-3 col-text font-weight-light p-0">Contattaci</h2>
+                <div class="col-6 p-0 mt-3 float-right">
                     <a href="#" class="float-right px-1">
                         <div class="text-center">
                             <img alt="Vai al profilo Instagram" src="/spaceair/res/icons/ig-w.svg" class="img-fluid">
@@ -35,38 +51,18 @@
                         </div>
                     </a>
                 </div>
-                <p class="font-weight-lighter col-text my-0 col-12">Telefono: 333-1212123</p>
-                <p class="font-weight-lighter col-text my-0 col-12">Indirizzo: Via dell’Università 14, Cesena (FC)</p>
             </div>
-        </div>
-    </section>  
-    <!--Google map-->
-    <section class="col-10 offset-1 mb-3 col-md-6 offset-md-3">
-        <div id="map-google" class="z-depth-1-half map-container">
-            <iframe src="https://maps.google.com/maps?q=Campus+di+Cesena+-+Università+di+Bologna+-+Alma+Mater+Studiorum&t=&z=13&ie=UTF8&iwloc=&output=embed" allowfullscreen></iframe>
-        </div>  
-    </section>    
-    <!--Contatti-->
-    <section class="row col-10 offset-1 mb-3 col-md-6 offset-md-3 p-0">
-        <div class="col-12 p-0">
-            <h2 class="mt-3 mb-3 col-text font-weight-light p-0">Contattaci</h2>
-            <form action="#">
-                <div class="row mb-3">
-                    <div class="col-12">
-                        <label for="inputMail">E-Mail</label>
-                        <input type="text" class="form-control" name="inputMail" id="inputMail" required/>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="container col-12">
-                        <label for="inputMsg">Messaggio</label>
-                        <textarea class="form-control" name="inputMsg" id="inputMsg" rows="5"></textarea>
-                    </div>
-                </div>
-                <button class="btn float-right col-dark col-back-white" type="submit">
-                    Invia
-                </button>
-            </form>
+            <ul>
+                <li>
+                    <a href="mailto:andrea.giulianelli4@studio.unibo.it" class="col-text">Andrea Giulianelli - andrea.giulianelli4@studio.unibo.it</a>
+                </li>
+                <li>
+                    <a href="mailto:simone.ceredi@studio.unibo.it" class="col-text">Simone Ceredi - simone.ceredi@studio.unibo.it</a>
+                </li>
+                <li>
+                    <a href="mailto:alessio.conti3@studio.unibo.it" class="col-text">Alessio Conti - alessio.conti3@studio.unibo.it</a>
+                </li>
+            </ul>
         </div>
     </section>    
     <!--Recensioni-->
